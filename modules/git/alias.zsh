8e6b383f15197e5acfe0a47ff4c2f33806d77513
John Winsor <johnwinsor@gmail.com>
2015-07-14 12:53:40 -0700

    initial changes to fork - fixed git alias (gs=git status). Added modules. set them to pure

--- modules/git/alias.zsh
+++ modules/git/alias.zsh
@@ -28,6 +28,7 @@ zstyle -s ':prezto:module:git:status:ignore' submodules '_git_status_ignore_subm
 
 # Git
 alias g='git'
+alias gs='git status'
 
 # Branch (b)
 alias gb='git branch'
@@ -143,7 +144,7 @@ alias gRs='git remote show'
 alias gRb='git-hub-browse'
 
 # Stash (s)
-alias gs='git stash'
+#alias gs='git stash'
 alias gsa='git stash apply'
 alias gsx='git stash drop'
 alias gsX='git-stash-clear-interactive'
